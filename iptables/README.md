15742382cd40225fe32c64c97248c2d48720c7da
Patrick Thoelken <patrick.thoelken@outlook.com>
2022-05-11 22:22:44 +0200

    init bash script

--- iptables/README.md
+++ iptables/README.md
@@ -1,5 +1,11 @@
-# Current State / Mode
-* in development / not released
+# Current State / Mode of blacklister.py
+* !!! in development / not released / DO NOT USE !!!
+
+# Current State / Mode of blacklister.sh
+* Tested on Debian 11
+
+# What is the difference between py / sh script?
+Really, not so much. The python script is currently in development and not ready to run on servers. The bash script I've scripted last night and it was much easier because I could adapt some old functions from further of my scripts like this.
 
 # How to work with "blacklister.py"?
 * `sudo apt update`
@@ -9,7 +15,15 @@
 * `sudo pip3 install -r requirements.txt`
 * `sudo python3 blacklister.py`
 
+# How to work with "blacklister.sh"?
+* `sudo apt update`
+* `sudo apt -y install python3 python3-setuptools python3-pip git iptables iptables-persistent`
+* `sudo git clone https://github.com/pthoelken/serverhardening.git`
+* `cd serverhardening/iptables`
+* `sudo chmod +x blacklister.sh`
+
 # How to run the blacklister automatically?
 * `sudo nano /etc/crontab`
 * Add in end of the file your crontab for example every night at midnight:
 * * `0 0 * * *   root    python3 /your/path/to/serverhardening/iptables/blacklister.py`
+* * `0 0 * * *   root    bash /your/path/to/serverhardening/iptables/blacklister.sh`
---
+++ iptables/blacklister.sh
@@ -0,0 +1,136 @@
+#!/bin/bash -e
+export PATH="/bin:/usr/bin:/sbin:/usr/sbin"
+
+strChainName=BLACKLIST
+strWorkingDirectory=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+strBlacklistFolder=$strWorkingDirectory/blacklists
+strLogFolder=$strWorkingDirectory/logs
+strConsoleLogFile=$strLogFolder/ip-blacklister-console.log
+strBlacklistFile=$strBlacklistFolder/blacklist
+strBlacklistTempFile=$strBlacklistFolder/tmp-blacklist
+strDDOSProtectionLockFile=$strWorkingDirectory/ddos.ruleset.lock
+
+intBlacklistingINPUTLineNo=$(iptables -n --list INPUT --line-numbers | grep -i $strChainName | sed -e 's/^\(.\{2\}\).*/\1/')
+
+function ConsoleLog() {
+
+    if [ ! -d $strLogFolder ]; then
+        mkdir -p $strLogFolder
+    fi
+
+    echo "[ $(date) ] | $1 " | tee -a $strConsoleLogFile
+}
+
+function ApplicationCheck(){
+    which "$1" | grep -o "$1" > /dev/null &&  return 0 || return 1 
+}
+
+function CheckRuntimePreparing(){
+    aryApplications=( iptables iptables-save ipset wget tee )
+    aryFolders=( $strBlacklistFolder )
+
+    for objApps in "${aryApplications[@]}"; do
+        if ( ! ApplicationCheck $objApps ); then
+            ConsoleLog "$objApps not found .. install ipset, iptables and iptables-save on your server"
+            exit 1
+        fi
+    done
+
+    for objFolders in "${aryFolders[@]}"; do
+        if [ ! -d $objFolders ]; then
+            mkdir -p $objFolders
+        fi
+    done
+}
+
+function PrepareBacklist() {
+    if [ -f $strBlacklistFile ]; then
+        rm -rf $strBlacklistFile
+    fi
+        wget http://lists.blocklist.de/lists/all.txt -O $strBlacklistTempFile
+        grep -v ":" $strBlacklistTempFile > $strBlacklistFile
+        rm -rf $strBlacklistTempFile
+}
+
+function ddosProectionRules() {
+    if [ ! -f $strDDOSProtectionLockFile ]; then
+
+        iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+        iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+        iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+        iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+        iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
+        iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
+        iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
+        iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
+        iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
+        iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
+        iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
+        iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
+        iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+        iptables -t mangle -A PREROUTING -p icmp -j DROP
+        iptables -t mangle -A PREROUTING -f -j DROP
+        iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset
+        iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+        iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+        iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
+        iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+        iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set
+        iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+        iptables -N port-scanning
+        iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+        iptables -A port-scanning -j DROP
+
+        echo "[ $(date) ] | Initialization of ddos protection rulset complete ..." | tee -a $strDDOSProtectionLockFile
+    else
+        echo "[ $(date) ] | Latest ddos protection rulset check ... protection already set ..." | tee -a $strDDOSProtectionLockFile
+    fi
+}
+
+ConsoleLog "Check runtime preparing for ip-blacklister ..."
+CheckRuntimePreparing
+
+ConsoleLog "Download new blacklists ..."
+PrepareBacklist >> /dev/null 2>&1
+
+ConsoleLog "Stopping fail2ban services ..."
+service fail2ban stop >> /dev/null 2>&1
+
+ConsoleLog "Flush or create ipset $strChainName table ..."
+if ! ipset flush $strChainName ; then
+    ipset create $strChainName hash:ip
+fi
+
+ConsoleLog "Importing blacklisted ip addresses from allblacklist ..."
+while IFS= read -r ipAddress
+do
+    ipset add $strChainName $ipAddress
+done < "$strBlacklistFile"
+
+ConsoleLog "Purging old blacklists ..."
+rm -rf $strBlacklistFolder/*.* >> /dev/null 2>&1
+
+ConsoleLog "Checking INPUT rule for inserting  of $strChainName on 1 ..."
+if [ -z "$intBlacklistingINPUTLineNo" ]; then
+    iptables -I INPUT 1 -m set --match-set $strChainName src -j DROP >> /dev/null 2>&1
+    ConsoleLog "Rule-set $strChainName successfully insert in chain INPUT to 1 ..."
+else
+    ConsoleLog "All fine, rule-set $strChainName already found in chain INPUT ..."
+fi
+
+ConsoleLog "Executing ddos procetion ruleset configuration ..."
+ddosProectionRules >> /dev/null 2>&1
+
+ConsoleLog "iptables-save makes iptables rules persistent ..."
+iptables-save >> /dev/null 2>&1
+
+ConsoleLog "Starting fail2ban services ..."
+service fail2ban start >> /dev/null 2>&1
+
+exit 0
